c670680a48fce72999128bf8c53fa4a16bee4782
Tim Howard <tim@timhoward.net>
2014-02-22 14:47:26 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webmentor::Application.config.secret_key_base = '317a9ba776ab85b3dd8f99f9ca4a9b58052bf90ce16a801b707a2526dd88e4245d416b50979d2362bc87dd78988506a7385251d76aa569e22dccd4fadf3d66c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Webmentor::Application.config.session_store :cookie_store, key: '_webmentor_session'
